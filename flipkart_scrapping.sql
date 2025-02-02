7576873c3425ee63159d457ab285dc62c678f111
singhvicky-03 <singhvicky3091@gmail.com>
2025-02-02 21:26:25 +0530

    Add files via upload

---
+++ flipkart_scrapping.sql
@@ -0,0 +1,13 @@
+create database flip;
+
+use flip;
+
+select * from flipkart_web_scrapping_
+where prices > 40000 and products like '%tv%' ;
+
+select * from flipkart_web_scrapping_
+where discounts >40;
+
+select * from flipkart_web_scrapping_
+where ratings > 4.5 and products like '%washing%';
+
